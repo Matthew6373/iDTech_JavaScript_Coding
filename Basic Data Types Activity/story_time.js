f4667ce0fa14af3ae1aa0e74198dac828061da19
Matthew6373 <justinmatthewt@gmail.com>
2022-06-24 12:18:55 +0700

    Completed

---
+++ Basic Data Types Activity/story_time.js
@@ -0,0 +1,32 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+let greetings = "Welcome to the Shadow Realm!";
+let name = 'Dante';
+let title = 'Demon Hunter';
+let enemy = 'Demon King';
+
+
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+let rivalryperiod = 150;
+let practiceperiod = 35;
+let age = 5;
+
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+let weaponsused = ['Sword', 'Shotgun', 'grenade'];
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+let hasFavWeapon = true;
+
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+console.log(greetings)
+console.log("I am" + " " + name + " " + "and known as the best" + " " + title)
+console.log("My long life goal is to kill the" + " " + enemy + " " + "who lives in the deepest level of the Shadow Realm")
+console.log("Ever since I was" + " " + age + " " + "I had been battling him")
+console.log("Now our rivalry has been going on for" + " " + rivalryperiod + " " + "years")
+console.log("I tried using all sorts of weapons against him but I found that using a" + " " + weaponsused[0] + " " + "was most effective")
+console.log("However it is" + " " + hasFavWeapon + " " + "and unfortunate that I have been practicing using a" + " " + weaponsused[1] + " " + "for" + " " + practiceperiod + " " + "years")
+console.log("Oh shoot!, the" + " " + enemy + " " + "is here. I'll talk again later. See you soon!")
